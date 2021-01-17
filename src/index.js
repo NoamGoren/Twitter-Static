d02cf85217a9dcdd670edb781b11ac5295a25f4e
Noam Goren <noamgoren@Noams-MacBook-Pro.local>
2021-01-17 11:06:14 +0000

    Further updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import moment from 'moment';
+
+function Tweet({tweet}) { return (
+    <div className="tweet">
+        <Avatar hash={tweet.gravatar}/>
+    <div className="content"> 
+    <Author author = {tweet.author}/>
+    <Time time = {tweet.timestamp}/> 
+    <Message text={tweet.message}/>
+<div className="buttons">
+          <ReplyButton/>
+          <RetweetButton/>
+          <LikeButton/>
+          <MoreOptionsButton/>
+        </div>
+      </div>
+</div>
+); }
+
+const testTweet = {
+    message: "I love cats",
+    gravatar: "xyz",
+    author : {
+        handle: "catperson",
+        name: "Dog Person"
+    },
+    likes : 2,
+    retweets: 0,
+    timestamp : "2016-07-30 21:24:37"
+};
+
+function Avatar({hash}) {
+    const url = `https://www.gravatar.com/avatar/${hash}`;
+    return(
+        <img
+        src={url} 
+        className="avatar"
+        alt="avatar" />
+    );  
+}
+
+function Message({text}) {
+    return(
+        <div className="message">
+           {text}
+        </div>
+    )
+}
+
+function Author({author}) {
+    const { name, handle } = author;
+    return (
+        <span className='author'>
+            <span className='name'>{name}</span>
+            <span className='handle'>@{handle}</span>
+        </span>
+    );
+}
+
+const Time = ({time}) => {
+    const timeString  = moment(time).fromNow();
+    return(
+        <span className="time"> {timeString}
+        </span>);
+    };
+    const ReplyButton = () => (
+    <i className="fa fa-reply reply-button"/>
+    );
+    const RetweetButton = () => (
+    <i className="fa fa-retweet retweet-button"/>
+    );
+    const LikeButton = () => (
+    <i className="fa fa-heart like-button"/>
+    );
+    const MoreOptionsButton = () => (
+    <i className="fa fa-ellipsis-h more-options-button"/>
+    );
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Tweet tweet={testTweet}/>,document.querySelector('#root'));
